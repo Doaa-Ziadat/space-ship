2cf9216398f0d49d04730e079d00b4ea5097af25
Doaa <doaaziadat@gmail.com>
2021-06-20 19:42:06 +0300

    first commit

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Destroy(gameObject, 3);
+    }
+}
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public GameObject Bulletprefap;
+    public Transform postionspwan;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+            Instantiate(Bulletprefap, postionspwan.transform.position, postionspwan.transform.rotation);
+
+        }
+    }
+}
